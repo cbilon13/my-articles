a3285a514be8d1961bbf959b2802dad42f428ffc
Charles <charles.bilon@ffufm.com>
2023-02-21 11:41:02 +0800

    create articles service

---
+++ frontend/src/app/core/services/articles/articles.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Articles } from "src/app/shared/models/articles.model";
+import { ArticlesRepository } from "../../repositories/articles.repository";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ArticlesService {
+    constructor(private articlesRepository: ArticlesRepository) {}
+
+    getArticles(): Observable<Articles[]> {
+        return this.articlesRepository.getArticles();
+    }
+}

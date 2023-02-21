0c6c1b4a08d1fc4effeaf2603ed8d459cc17489a
Charles <charles.bilon@ffufm.com>
2023-02-21 11:42:30 +0800

    display articles data from api to template

--- frontend/src/app/articles/articles.component.ts
+++ frontend/src/app/articles/articles.component.ts
@@ -1,10 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ArticlesService } from "../core/services/articles/articles.service";
+import { Articles } from "../shared/models/articles.model";
 
 @Component({
     selector: 'my-articles',
     templateUrl: './articles.component.html',
     styleUrls: ['./articles.component.scss']
 })
-export class ArticlesComponent {
-    constructor() { console.log('ArticlesComponent') }
+export class ArticlesComponent implements OnInit {
+    articles!: Articles[];
+
+    constructor(private articlesService: ArticlesService) { console.log('ArticlesComponent') }
+    // trackBy: identify
+    ngOnInit(): void {
+        this.articlesService.getArticles().subscribe(articles => this.articles = articles);
+    }
 }

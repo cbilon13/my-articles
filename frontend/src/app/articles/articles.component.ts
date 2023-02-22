82a1eba35ed42686194ccc93350e2afeac6bf820
Charles <charles.bilon@ffufm.com>
2023-02-22 23:11:07 +0800

    optimize usage of scss tyling

--- frontend/src/app/articles/articles.component.ts
+++ frontend/src/app/articles/articles.component.ts
@@ -9,34 +9,27 @@ import { Articles } from "../shared/models/articles.model";
 })
 export class ArticlesComponent implements OnInit {
     articles!: Articles[];
-    isCreate: boolean = false;
+    isOpenAddEditForm: boolean = false;
 
     selectedArticle!: Articles;
     allArticles!: Articles[];
-    constructor(private articlesService: ArticlesService) { console.log('ArticlesComponent') }
+    constructor(private articlesService: ArticlesService) {  }
     // trackBy: identify
     ngOnInit(): void {
         this.articlesService.getArticles().subscribe(articles => {
             this.articles = articles;
             this.allArticles = articles;
+
+            const test = articles[0];
+            console.log(new Date(test.date));
         });
     }
 
     filter(articles: Articles[] | null): void {
-        console.log(this.allArticles);
         this.articles = articles ?? this.allArticles;
     }
 
-    edit(article: Articles): void {
-        this.isCreate = true;
-        this.selectedArticle = article;
-    }
-
-    create(state: boolean): void {
-        this.isCreate = state;
-    }
-
-    close(state: boolean): void {
-        this.isCreate = !state;
+    openAddEditForm() {
+        this.isOpenAddEditForm = !this.isOpenAddEditForm;
     }
 }

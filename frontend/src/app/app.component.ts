bfda8b01b29dc970a53eb2e46a139c96c520eec8
Charles <charles.bilon@ffufm.com>
2023-02-22 23:04:44 +0800

    add login guard

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,12 +10,12 @@ export class AppComponent implements OnInit {
   title = 'frontend';
 
   ngOnInit(): void {
-    fetch('http://localhost:3000/articles')
-      .then(res => res.json())
-      .then(res => console.log(res));
+    // fetch('http://localhost:3000/articles')
+    //   .then(res => res.json())
+    //   .then(res => console.log(res));
 
-    fetch('http://localhost:3000/users')
-      .then(res => res.json())
-      .then(res => console.log(res));
+    // fetch('http://localhost:3000/users')
+    //   .then(res => res.json())
+    //   .then(res => console.log(res));
   }
 }
